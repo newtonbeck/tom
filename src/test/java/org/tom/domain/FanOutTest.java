9aa070c4aea4ba37290a0379a893d726d2892e3b
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2022-09-02 18:09:17 +0200

    Creates fan out tests

---
+++ src/test/java/org/tom/domain/FanOutTest.java
@@ -0,0 +1,22 @@
+package org.tom.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FanOutTest {
+
+    @Test
+    public void new_fan_out_should_be_created_with_value_0_by_default() {
+        assertEquals(new FanOut(0), new FanOut());
+    }
+
+    @Test
+    public void fan_out_should_be_increased_by_1_when_increase_is_invoked() {
+        var fanOut = new FanOut();
+        fanOut.increase();
+
+        assertEquals(new FanOut(1), fanOut);
+    }
+
+}
